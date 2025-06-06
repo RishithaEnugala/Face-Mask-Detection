90bf1caa1313cda4d9af3b405afa4750d07b0e77
Rishitha Reddy Enugala <139004213+RishithaEnugala@users.noreply.github.com>
2025-06-07 01:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,35 +6,21 @@ import re
 import numpy as np
 from flask import jsonify
 
-# Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
 
-# Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
 from PIL import Image
-# Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
 MODEL_PATH = 'ourmodel.h5'
 
-# Load your trained model
 model = load_model(MODEL_PATH) 
-model.make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-# print('Model loaded. Check http://127.0.0.1:5000/')
-
+model.make_predict_function()       
 
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(128, 128))
@@ -52,29 +38,20 @@ def model_predict(img_path, model):
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # Get the file from post request
         f = request.files['file']
 
-        # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
-        # Make prediction
         pcn = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        # result = str(pred_class[0][0][1])               # Convert to string
 
         if pcn ==0:
             result = "Person is not having mask"
